c22fd89916258f9d31498e01670a3c1c2da3debb
TheMammothManRemastered <54153096+TheMammothManRemastered@users.noreply.github.com>
2022-03-26 20:56:47 -0400

    removed some unneeded files

--- src/Bain.java
+++
@@ -1,8 +0,0 @@
-public class Bain {
-
-    public static void minecraft() {
-        System.out.println(
-        "baiiiiiin i'm in minecraft baiiiin baig im in baiiiin bain medic bag s");
-    }
-
-}
